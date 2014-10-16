1bd939b4b1695aeb0977427b8f26b3315490d580
Torstein Krause Johansen <torstein.k.johansen@gmail.com>
2014-10-16 19:59:36 +0200

    JBoss CLI script now adds a JMS topic too
    - introduced variable.

--- src/main/jboss/add-jms-channels.sh
+++ src/main/jboss/add-jms-channels.sh
@@ -3,11 +3,15 @@
 # by torstein.k.johansen@conduct.no
 
 jboss_cli_connect="/opt/jboss-eap-6.2/bin/jboss-cli.sh -c"
-datasource_name=moccasin-ds
+name_prefix=buffalo
 
 ${jboss_cli_connect} <<EOF
 jms-queue add \
-  --queue-address=buffaloQueue \
-  --entries=queue/buffalo,java:jboss/jms/queue/buffalo
+  --queue-address=${name_prefix}Queue \
+  --entries=queue/${name_prefix},java:jboss/jms/queue/${name_prefix}
+
+jms-topic add \
+  --topic-address=${name_prefix}Topic \
+  --entries=topic/${name_prefix},java:jboss/jms/topic/${name_prefix}
 EOF
 
